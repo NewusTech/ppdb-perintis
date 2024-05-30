af8605e62125de04db04773fd3e3f56c3b25d23a
raiszidane95 <raiszidane95@gmail.com>
2024-05-30 16:32:09 +0700

    Add filter Daftar Siswa

--- app/Http/Livewire/Tables/DaftarSiswaTable.php
+++ app/Http/Livewire/Tables/DaftarSiswaTable.php
@@ -6,10 +6,24 @@ use App\Models\Pendaftaran;
 use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelLivewireTables\Views\Filter;
 
 class DaftarSiswaTable extends DataTableComponent
 {
     public $kelas_id;
+    public $keterangan = '';
+
+    public function filters(): array
+    {
+        return [
+            'tahun_masuk' => Filter::make('Tahun')->select([
+                '' => 'Semua',
+                '2024' => '2024',
+                '2023' => '2023',
+                '2022' => '2022',
+            ]),
+        ];
+    }
 
     public function columns(): array
     {
@@ -38,7 +52,8 @@ class DaftarSiswaTable extends DataTableComponent
             ->join('kelas', 'kelas.id', '=', 'pendaftaran.kelas_id')
             ->select('pendaftaran.*', 'users.name', 'users.username')
             ->where('kelas_id', '=', $this->kelas_id)
-            ->where('status_daftar_ulang', '=', 1);
+            ->where('status_daftar_ulang', '=', 1)
+            ->when($this->getFilter('tahun_masuk'), fn ($query, $created_at) => $query->whereYear('pendaftaran.created_at', $created_at));
 
         return $a;
     }
